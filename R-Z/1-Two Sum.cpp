8c5e5694e8fe8460ddcbffc7192e0ba155a2b7b9
sczyh30 <sczyh16@gmail.com>
2015-08-08 21:20:33 +0800

    change the dir
    change the dir
    2015.8.8

---
+++ R-Z/1-Two Sum.cpp
@@ -0,0 +1,36 @@
+/**
+ O(n2) runtime, O(1) space - Brute force
+ O(n) runtime, O(n) space - Hash table:
+ We could reduce the runtime complexity of looking up a value to O(1) 
+ using a hash map that maps a value to its index.
+ 
+ My first solution:
+ first sort the vector, then use two flags to traverse the vector
+ and get the flag from inital vector
+**/
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+    	vector<int> v(nums);
+    	sort(v.begin(), v.end());
+    
+    	int p1 = 0, p2 = v.size() - 1;
+    	while (p1 < p2) {
+    		if (v[p1] + v[p2] == target)
+    			break;
+    		else if (v[p1] + v[p2] > target)
+    			--p2;
+    		else
+    			++p1;
+    	}
+    	vector<int> index;
+    	for (int i = 0, pc = 2; i < nums.size(); ++i) {
+    		if (v[p1] == nums[i] || v[p2] == nums[i]) {
+    			index.push_back(i + 1);
+    			if (--pc == 0)
+    				break;
+    		}
+    	}
+        return index;
+    }
+};
